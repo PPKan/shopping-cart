3eb486c9e4663c156c4263dbcfd708733a81d37f
Peter <petervsjim@gmail.com>
2022-01-23 20:31:26 +0800

    checkout payment update

--- src/component/Checkout.js
+++ src/component/Checkout.js
@@ -38,9 +38,11 @@ export default function Checkout(props) {
               <label htmlFor="email">Email Address</label>
               <input required type="email" name="email"></input>
               <label htmlFor="card">Credit Card</label>
-              <input required type="text" name="card" pattern="[0-9]*" inputmode="numeric"></input>
+              <input required type="text" name="card" pattern="[0-9]*" inputmode="numeric" placeholder="0001 0001 0001 0001"></input>
+              <label htmlFor="date">Expire Date</label>
+              <input required type="text" name="date" placeholder="01/2025"></input>
               <label htmlFor="security">Security Code</label>
-              <input required type="text" name="security" min="1" max="999"></input>
+              <input required type="text" name="security" min="1" max="999" placeholder="001"></input>
             </div>
             <button className="btn" type="submit">
               Purchase
